8cac578c6df817059b1dbb09b9582e6d910f5de3
DFGilart <151823418+DFGilart@users.noreply.github.com>
2023-11-23 09:31:25 +0100

    Add files via upload

---
+++ argumentos/src/main/java/com/mycompany/argumentos/Argumentos.java
@@ -0,0 +1,26 @@
+
+
+package com.mycompany.argumentos;
+
+
+public class Argumentos {
+
+    public static void main(String[] args) {
+        String argumento1;
+        String argumento2;
+        
+        argumento1 = args[0];
+        argumento2 = args[1];
+        System.out.println("Argumento 1:" + argumento1);
+        System.out.println("Argumento 2:" + argumento2);
+        switch(argumento){
+            case "1":{
+                System.out.println("Idioma Español");
+                break;
+            }
+            case "2:"{
+                
+            }
+        }
+    }
+}
